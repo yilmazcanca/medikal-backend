2c9faf85172e0ccb4923c69ddd7bb1335d3dcee8
Yılmaz <yilmazcanca@gmail.com>
2025-12-12 19:34:34 +0300

    Cloudinary

--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -1,45 +1,37 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = [
-    'strapi::logger',
-    'strapi::errors',
-    {
-        name: 'strapi::security',
-        config: {
-            contentSecurityPolicy: {
-                useDefaults: true,
-                directives: {
-                    'connect-src': ["'self'", 'https:'],
-                    'img-src': [
-                        "'self'",
-                        'data:',
-                        'blob:',
-                        'dl.airtable.com',
-                        'res.cloudinary.com',
-                    ],
-                    'media-src': [
-                        "'self'",
-                        'data:',
-                        'blob:',
-                        'dl.airtable.com',
-                        'res.cloudinary.com',
-                    ],
-                    upgradeInsecureRequests: null,
-                },
-            },
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'res.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
         },
+      },
     },
-    {
-        name: 'strapi::cors',
-        config: {
-            headers: '*',
-            origin: ['*']
-        }
-    },
-    'strapi::poweredBy',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
