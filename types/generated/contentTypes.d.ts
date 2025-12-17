4115ff322ced74bbc4d6c7b6b499e3c4acb2a7a5
Yılmaz <yilmazcanca@gmail.com>
2025-12-17 21:48:48 +0300

    One cikan urun duzeltmeleri yapildi

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -552,6 +552,7 @@ export interface ApiOneCikanOneCikan extends Struct.SingleTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    UzunAciklama: Schema.Attribute.Blocks;
   };
 }
 
